f06dedbad590ca5e5be9eb6d16a4e988b6e65854
Andrey Tregubenko <tregubenko.work@gmail.com>
2018-06-20 10:45:41 +0300

    Prod Fixes

--- components/RestFul.php
+++ components/RestFul.php
@@ -22,11 +22,6 @@ class RestFul extends \yii\rest\ActiveController
         $this->lang = $this->getOldLangAssoc(Yii::$app->language);
         $this->coreSettings = self::getCoreSettings();
 
-        echo '<pre>' . print_r([
-            Yii::$app->request->getHeaders(),
-            apache_request_headers()
-        ], true) . '</pre>'; exit;
-
         $authHeader = Yii::$app->request->getHeaders()->get('Authorization');
         if ($authHeader !== null && preg_match('/^Bearer\s+(.*?)$/', $authHeader, $matches)) $this->auth_token = $matches[1];
         else throw new HttpException(403, 'Authorization required!');
