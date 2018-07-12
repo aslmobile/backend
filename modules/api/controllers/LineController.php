f915e6dc660bc79dd6221c7338ad2e11c888b284
Andrey Tregubenko <tregubenko.work@gmail.com>
2018-07-12 19:46:35 +0300

    Driver methods

--- modules/api/controllers/LineController.php
+++ modules/api/controllers/LineController.php
@@ -1,5 +1,8 @@
 <?php namespace app\modules\api\controllers;
 
+use app\models\Checkpoint;
+use app\models\Line;
+use app\modules\api\models\Trip;
 use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
@@ -43,5 +46,121 @@ class LineController extends BaseController
                 ]
             ]
         ];
+    }
+
+    public function actionStartpoints()
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $points = [];
+
+        $startpoints = Checkpoint::find()->where(['type' => Checkpoint::TYPE_START])->all();
+        if ($startpoints && count($startpoints) > 0) foreach ($startpoints as $point)
+        {
+            /** @var $point \app\models\Checkpoint */
+            $points[] = $point->toArray();
+        }
+
+        $this->module->data = $points;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionEndpoints()
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $points = [];
+
+        $endpoints = Checkpoint::find()->where(['type' => Checkpoint::TYPE_END])->all();
+        if ($endpoints && count($endpoints) > 0) foreach ($endpoints as $point)
+        {
+            /** @var $point \app\models\Checkpoint */
+            $points[] = $point->toArray();
+        }
+
+        $this->module->data = $points;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionCheckpoints()
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $points = [];
+
+        $checkpoints = Checkpoint::find()->where(['type' => Checkpoint::TYPE_STOP])->all();
+        if ($checkpoints && count($checkpoints) > 0) foreach ($checkpoints as $point)
+        {
+            /** @var $point \app\models\Checkpoint */
+            $points[] = $point->toArray();
+        }
+
+        $this->module->data = $points;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionSeats($id)
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        /** @var \app\models\Line $line */
+        $lines = Line::find()->andWhere(['AND', ['=', 'route_id', $id]])->all();
+        if (!$lines) $this->module->setError(422, '_line', "Not Found");
+
+        $seats = 0;
+        $free_seats = 0;
+
+        foreach ($lines as $line)
+        {
+            $seats += $line->seats;
+            $free_seats += $line->freeseats;
+        }
+
+        $this->module->data['seats'] = $seats;
+        $this->module->data['free_seats'] = $free_seats;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionPassengers($id)
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $passengers = Trip::find()->where(['route_id' => $id])->all();
+        if (!$passengers) $this->module->setError(422, 'trip', "Not Found");
+
+        $_passengers = [];
+        foreach ($passengers as $passenger) $_passengers[] = $passenger->toArray();
+
+        $this->module->data['passengers'] = $_passengers;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionUpdateLine($id)
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $line = $this->getLine($id);
+        if (!$line) $this->module->setError(422, '_line', "Not Found");
+
+        $line->freeseats = 0;
+        $line->seats = 0;
+
+        $line->save();
+    }
+
+    protected function getLine($line_id)
+    {
+        return Line::findOne($line_id);
     }
 }
--- modules/api/controllers/TripController.php
+++ modules/api/controllers/TripController.php
@@ -1,5 +1,7 @@
 <?php namespace app\modules\api\controllers;
 
+use app\models\Line;
+use app\models\Route;
 use app\modules\api\models\Trip;
 use Yii;
 use yii\filters\AccessControl;
@@ -29,6 +31,9 @@ class TripController extends BaseController
                 'rules' => [
                     [
                         'actions' => [
+                            'test',
+                            'calculate-passenger-tariff',
+
                             'passenger-comments',
                             'driver-comments',
                             'trips'
@@ -102,6 +107,79 @@ class TripController extends BaseController
         $this->module->data = $trips;
         $this->module->setSuccess();
         $this->module->sendResponse();
+    }
+
+    public function actionTest($id)
+    {
+        $tariff = $this->calculatePassengerTariff($id);
+
+        $this->module->data['tariff'] = $tariff;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    protected function getRate($route_id)
+    {
+        /** @var \app\models\Line $line */
+        $lines = Line::find()->andWhere([
+            'AND',
+            ['=', 'route_id', $route_id]
+        ])->all();
+
+        if (!$lines) $this->module->setError(422, '_line', "Not Found");
+
+        $seats = 0;
+        foreach ($lines as $line) $seats += $line->freeseats;
+
+        $passengers = Trip::find()->andWhere([
+            'AND',
+            ['=', 'route_id', $route_id],
+            ['=', 'driver_id', 0]
+        ])->count();
+
+        if ($seats == 0) $rate = 1.5;
+        elseif ($passengers == 0) $rate = 1;
+        else
+        {
+            $hard_rate = round($passengers / $seats, 2);
+
+            if ($hard_rate >= .35 && $hard_rate <= .6) $rate = 1.1;
+            elseif ($hard_rate >= .6 && $hard_rate <= .7) $rate = 1.2;
+            elseif ($hard_rate >= .7 && $hard_rate <= .8) $rate = 1.3;
+            elseif ($hard_rate >= .8 && $hard_rate <= .9) $rate = 1.4;
+            else $rate = 1.5;
+        }
+
+        return $rate;
+    }
+
+    protected function calculatePassengerTariff($id)
+    {
+        $rate = $this->getRate($id);
+        $taxi_tariff = 0;
+
+        /** @var \app\models\Route $route */
+        $route = Route::find()->where(['id' => $id])->one();
+        if (!$route) $this->module->setError(422, '_route', "Not Found");
+
+        $tariff = $route->base_tariff * $rate + $taxi_tariff;
+
+        return [
+            'base_tariff' => $route->base_tariff,
+            'tariff' => $tariff
+        ];
+    }
+
+    protected function calculateDriverTariff()
+    {
+        /**
+         * Расчет тарифа по зависимостям:
+         * - Базовый тариф
+         * - Задолженость
+         * - Спрос
+         *
+         * ((кол-во пассажиров / кол-во мест) * базовый тариф) * коофициент + задолженость
+         */
     }
 
     protected function getTrips($id, $type = 'user')
