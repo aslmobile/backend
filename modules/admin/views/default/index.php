0e328e2a7d4a5744d31d227b23d5a74070eececb
Andrey Tregubenko <tregubenko.work@gmail.com>
2018-07-18 12:52:04 +0300

    Dashboard alpha

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -13,4 +13,45 @@ $statuses = Yii::$app->params['statuses'];
 
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
+    <div class="container-fluid">
+        <h2><?= Yii::t('app' , "Панель управления"); ?></h2>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua"><i class="fa fa-car"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><?= Yii::t('app', "Зарегистрировано автомобилей"); ?></span>
+                        <span class="info-box-number"><?= \app\models\Vehicles::find()->count(); ?></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua"><i class="fa fa-user-secret"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><?= Yii::t('app', "Зарегистрировано водителей"); ?></span>
+                        <span class="info-box-number"><?= \app\models\User::find()->where(['type' => \app\modules\api\models\Users::TYPE_DRIVER])->count(); ?></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua"><i class="fa fa-users"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><?= Yii::t('app', "Зарегистрировано пассажиров"); ?></span>
+                        <span class="info-box-number"><?= \app\models\User::find()->where(['type' => \app\modules\api\models\Users::TYPE_PASSENGER])->count(); ?></span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="info-box">
+                    <span class="info-box-icon bg-aqua"><i class="fa fa-arrows-h"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text"><?= Yii::t('app', "Совершено поездок"); ?></span>
+                        <span class="info-box-number"><?= \app\models\Trip::find()->count(); ?></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
