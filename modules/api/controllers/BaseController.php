eba04a179bce93e9bd3aa2c81932d18a3ca91866
Andrey Tregubenko <tregubenko.work@gmail.com>
2018-08-03 12:55:43 +0300

    SP4

--- modules/api/controllers/BaseController.php
+++ modules/api/controllers/BaseController.php
@@ -163,6 +163,7 @@ class BaseController extends RestFul
         $device = false;
         if (isset ($this->device_id) && !empty($this->device_id))
             $device = Devices::findOne(['device_id' => $this->device_id]);
+        if ($device->user->phone != $this->body->phone) $device = false;
 
         if (!$device && !empty ($this->body->phone))
         {
