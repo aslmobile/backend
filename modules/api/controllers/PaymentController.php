e38f6c49ca9028960ca7e8ad9fb4e45e515ebb33
Andrey Tregubenko <tregubenko.work@gmail.com>
2018-08-16 12:44:32 +0300

    SP4

--- modules/api/controllers/PaymentController.php
+++ modules/api/controllers/PaymentController.php
@@ -1,5 +1,6 @@
 <?php namespace app\modules\api\controllers;
 
+use app\models\Transactions;
 use Yii;
 use yii\filters\AccessControl;
 use yii\filters\VerbFilter;
@@ -28,20 +29,61 @@ class PaymentController extends BaseController
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'cities'  => ['GET'],
+                    'transactions'  => ['POST'],
+                    'transaction'  => ['GET'],
+                    'methods'  => ['GET'],
                 ]
             ]
         ];
     }
 
-    public function actionCities()
+    public function actionTransactions()
     {
         $user = $this->TokenAuth(self::TOKEN);
         if ($user) $user = $this->user;
 
-        $cities = City::getCitiesList(2);
+        $this->prepareBody();
+        $this->validateBodyParams(['timestamp_min', 'timestamp_max']);
 
-        $this->module->data = $cities;
+        $timestamp_min = intval($this->body->timestamp_min);
+        $timestamp_max = intval($this->body->timestamp_max);
+
+        $transactions = Transactions::find()->andWhere([
+            'AND',
+            ['between', 'created_at', $timestamp_min, $timestamp_max],
+            ['=', 'user_id', $user->id]
+        ])->orderBy(['created_at' => SORT_DESC])->all();
+
+        $transactions_data = [];
+        if ($transactions && count($transactions) > 0) foreach ($transactions as $transaction)
+        {
+            $transactions_data = $transaction->toArray();
+        }
+
+        $this->module->data['transactions'] = $transactions_data;
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionTransaction($id)
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $transaction = Transactions::findOne($id);
+        if (!$transaction) $this->module->setError(422, 'transaction', Yii::$app->mv->gt("Не найдена", [], false));
+
+        $this->module->data = $transaction->toArray();
+        $this->module->setSuccess();
+        $this->module->sendResponse();
+    }
+
+    public function actionMethods()
+    {
+        $user = $this->TokenAuth(self::TOKEN);
+        if ($user) $user = $this->user;
+
+        $this->module->data = Transactions::getPaymentMethods();
         $this->module->setSuccess();
         $this->module->sendResponse();
     }
