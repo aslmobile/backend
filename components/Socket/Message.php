8d601343bfaa2ab9ec32674caa47cadda671a362
Andrey Tregubenko <tregubenko.work@gmail.com>
2018-08-22 11:59:44 +0300

    SP4

--- components/Socket/Message.php
+++ components/Socket/Message.php
@@ -68,7 +68,7 @@ class Message
 
         if (isset ($data['data']['message_id'])) $this->message_id = intval($data['data']['message_id']);
 
-        $lines = Line::find()->where(['status' => Line::STATUS_QUEUE])->orderBy(['freeseats' => SORT_DESC, 'created_at' => SORT_DESC])->all();
+        $lines = Line::find()->where(['status' => Line::STATUS_QUEUE, 'driver_id' => $device->user_id])->orderBy(['freeseats' => SORT_DESC, 'created_at' => SORT_DESC])->all();
         $queue = [];
         foreach ($lines as $line) $queue[] = $line->toArray();
 
