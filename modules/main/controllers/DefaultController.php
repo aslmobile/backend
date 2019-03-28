f0a9b33ce67a742d940260abb54294c5eadaeddd
vitarr <liervitarr@gmail.com>
2019-03-28 14:08:34 +0200

    dynamic

--- modules/main/controllers/DefaultController.php
+++ modules/main/controllers/DefaultController.php
@@ -28,14 +28,12 @@ class DefaultController extends Controller
     {
         if (!$page) {
             if (Yii::$app->user->isGuest) return $this->redirect(Url::toRoute('/user/default/login/'));
-
             return $this->redirect(Url::toRoute('/admin/'));
         }
 
-        $model = Dynamic::find()
-            ->multilingual()
-            ->where(['url' => $page])
-            ->one();
+        $this->layout = '@app/views/layouts/empty_page';
+
+        $model = Dynamic::find()->multilingual()->where(['url' => $page])->one();
 
         if ($model === null) throw new NotFoundHttpException(Yii::t('app', 'Запрашиваемая страница не найдена.'));
 
@@ -46,32 +44,4 @@ class DefaultController extends Controller
     {
         return $this->render('page', ['model' => $page]);
     }
-
-    public function actionUbermodelextender()
-    {
-        if (Yii::$app->user->isGuest || !Yii::$app->user->can('admin')) return $this->goBack();
-        $f_name = Yii::$app->request->get('model');
-        $module = Yii::$app->request->get('module');
-        if (!$module) $module = 'admin';
-        $r_dir = $_SERVER['DOCUMENT_ROOT'] . '/modules/' . $module . '/models/';
-        $m_dir = $_SERVER['DOCUMENT_ROOT'] . '/models/';
-        if ($f_name) $f_name = ucfirst($f_name);
-
-        if ($f_name && file_exists($m_dir . $f_name . '.php') && !file_exists($r_dir . $f_name . '.php')) {
-            $new_file = <<<HTML
-<?php
-
-namespace app\modules\\$module\models;
-
-
-class $f_name extends \app\models\\$f_name
-{
-
-}
-HTML;
-            file_put_contents($r_dir . $f_name . '.php', $new_file);
-        } else die('uber fail');
-
-    }
-
 }
